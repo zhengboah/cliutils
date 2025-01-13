584b5a6764b185217b4feaa1bd4d8e52d268709b
zhengbo <zhengb@guance.com>
2025-01-13 16:41:38 +0800

    save

--- dialtesting/multi_test.go
+++ dialtesting/multi_test.go
@@ -11,95 +11,145 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestMulti(t *testing.T) {
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("OK"))
-	}))
-
-	step := HTTPTask{
-		URL: server.URL + "?token={{config_var_token}}-{{config_var_global}}",
-		PostScript: `
-			result["is_failed"] = false	
-			vars["token"] = "token"
-		`,
+	body := struct {
+		Token string
+	}{
+		Token: fmt.Sprintf("token_%d", time.Now().UnixNano()),
 	}
 
-	step1Bytes, _ := json.Marshal(step)
+	engine := gin.Default()
+	engine.GET("/token", func(ctx *gin.Context) {
+		bodyBytes, _ := json.Marshal(body)
+		ctx.Writer.Write(bodyBytes)
+	})
 
-	step = HTTPTask{
-		URL: fmt.Sprintf("%s?token={{token}}-{{config_var_token}}-{{config_var_global}}", server.URL),
-		PostScript: `
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		engine.ServeHTTP(w, r)
+	}))
+	defer server.Close()
+
+	cases := makeCases(server.URL)
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			taskString, _ := json.Marshal(tc.Task)
+			task, err := NewTask(string(taskString), tc.Task)
+
+			assert.NoError(t, err)
+
+			globalVars := map[string]Variable{
+				"global_var_id": {
+					Value: "global_var_value",
+				},
+			}
+			assert.NoError(t, task.RenderTemplateAndInit(globalVars))
+
+			assert.NoError(t, task.Run())
+
+			tags, fields := task.GetResults()
+			if tc.IsFailed {
+				assert.Equal(t, -1, fields["success"])
+			} else {
+				assert.Equal(t, 1, fields["success"])
+			}
+			assert.NotNil(t, tags, fields)
+			assert.NoError(t, tc.Check(t, tags, fields))
+		})
+	}
+}
+
+type cs struct {
+	Name       string
+	Task       *MultiTask
+	IsFailed   bool
+	Check      func(t assert.TestingT, tags map[string]string, fields map[string]interface{}) error
+	GlobalVars map[string]Variable
+}
+
+func makeCases(serverURL string) []cs {
+	return []cs{
+		{
+			Name:     "normal test",
+			IsFailed: true,
+			Task: func() *MultiTask {
+				step1 := HTTPTask{
+					URL: serverURL + "/token?token={{config_var_token}}-{{config_var_global}}",
+					PostScript: `
+			result["is_failed"] = false	
+			body = load_json(response["body"])
+			vars["token"] = body["Token"]
+		`,
+				}
+
+				step1Bytes, _ := json.Marshal(step1)
+
+				step3 := HTTPTask{
+					URL: fmt.Sprintf("%s/token?token={{token}}", serverURL),
+					PostScript: `
 			result["is_failed"] = true
 			result["error_message"]	= "error"
 		`,
-	}
+				}
 
-	step2Bytes, _ := json.Marshal(step)
-
-	multiTask := &MultiTask{
-		Task: &Task{
-			ConfigVars: []*ConfigVar{
-				{
-					Name:  "config_var_token",
-					Value: "config_var_token",
-				},
-				{
-					Name: "config_var_global",
-					ID:   "global_var_id",
-					Type: TypeVariableGlobal,
-				},
-			},
-		},
-		Steps: []*MultiStep{
-			{
-				Type:       "http",
-				TaskString: string(step1Bytes),
-				ExtractedVars: []MultiExtractedVar{
-					{
-						Name:  "token",
-						Field: "token",
+				step2Bytes, _ := json.Marshal(step3)
+				return &MultiTask{
+					Task: &Task{
+						ConfigVars: []*ConfigVar{
+							{
+								Name:  "config_var_token",
+								Value: "config_var_token",
+							},
+							{
+								Name: "config_var_global",
+								ID:   "global_var_id",
+								Type: TypeVariableGlobal,
+							},
+						},
 					},
+					Steps: []*MultiStep{
+						{
+							Type:       "http",
+							TaskString: string(step1Bytes),
+							ExtractedVars: []MultiExtractedVar{
+								{
+									Name:  "token",
+									Field: "token",
+								},
+							},
+						},
+						{
+							Type:  "wait",
+							Value: 1,
+						},
+						{
+							Type:       "http",
+							TaskString: string(step2Bytes),
+						},
+					},
+				}
+			}(),
+			GlobalVars: map[string]Variable{
+				"global_var_id": {
+					Value: "global_var_value",
 				},
 			},
-			{
-				Type:  "wait",
-				Value: 1,
-			},
-
-			{
-				Type:       "http",
-				TaskString: string(step2Bytes),
+			Check: func(t assert.TestingT, tags map[string]string, fields map[string]interface{}) error {
+				assert.Equal(t, "FAIL", tags["status"])
+				msg := map[string]interface{}{}
+				message, ok := fields["message"].(string)
+				assert.True(t, ok)
+				assert.NoError(t, json.Unmarshal([]byte(message), &msg))
+				assert.Equal(t, "error", msg["fail_reason"])
+				assert.EqualValues(t, -1, fields["success"])
+				return nil
 			},
 		},
 	}
-
-	taskString, _ := json.Marshal(multiTask)
-
-	task, err := NewTask(string(taskString), multiTask)
-
-	assert.NoError(t, err)
-
-	globalVars := map[string]Variable{
-		"global_var_id": {
-			Value: "global_var_value",
-		},
-	}
-	assert.NoError(t, task.RenderTemplateAndInit(globalVars))
-
-	assert.NoError(t, task.Run())
-
-	tags, fields := task.GetResults()
-
-	assert.True(t, tags != nil)
-	assert.True(t, fields != nil)
-
-	assert.Equal(t, "FAIL", tags["status"])
-	assert.EqualValues(t, -1, fields["success"])
-	assert.Equal(t, 3, len(multiTask.Steps))
-	assert.Equal(t, fmt.Sprintf("%s?token=config_var_token-global_var_value", server.URL), multiTask.Steps[0].result["url"])
-	assert.Equal(t, fmt.Sprintf("%s?token=token-config_var_token-global_var_value", server.URL), multiTask.Steps[2].result["url"])
 }
